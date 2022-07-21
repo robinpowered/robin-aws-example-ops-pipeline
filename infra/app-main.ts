2f6d6c0c4796167aabf255e17d4cab8eb5b569f5
Jeremy Slottje <jeremy.slottje@robinpowered.com>
2022-07-21 14:56:46 -0400

    remove loadtest stack

--- infra/app-main.ts
+++ infra/app-main.ts
@@ -3,7 +3,6 @@ import { AppContext, AppContextError } from '../lib/template/app-context';
 
 import { VpcInfraStack } from './common-infra/vpc-infra-stack';
 import { EcsAlbServiceStack } from './ecs-service/ecs-alb-service-stack';
-import { EcsCommonServiceStack } from './ecs-service/ecs-common-service-stack';
 
 
 try {
@@ -16,7 +15,6 @@ try {
     new EcsAlbServiceStack(appContext, appContext.appConfig.Stack.SampleBackendFastapi);
     new EcsAlbServiceStack(appContext, appContext.appConfig.Stack.SampleFrontendFlask);
 
-    new EcsCommonServiceStack(appContext, appContext.appConfig.Stack.LoadTesterScript);
 } catch (error) {
     if (error instanceof AppContextError) {
         console.error('[AppContextError]:', error.message);
